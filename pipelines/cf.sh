3bcae70dee463f5f6d6a88d612c54e7734f9da39
Ruben Koster <rkoster@starkandwayne.com>
2017-04-05 11:22:42 +0200

    Added mysql pipeline example

--- pipelines/cf.sh
+++
@@ -1,5 +0,0 @@
-fly -t bucc set-pipeline -c ./pipelines/cf.yml -p cf \
-    --var "bosh_target=${BOSH_ENVIRONMENT}" \
-    --var "bosh_client=${BOSH_CLIENT}" \
-    --var "bosh_client_secret=${BOSH_CLIENT_SECRET}" \
-    --var "bosh_ca_cert=${BOSH_CA_CERT}"
